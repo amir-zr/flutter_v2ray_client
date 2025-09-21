ac2194a08e44678afd5b7ee930a3a147fcf103dd
shayanheidari01 <shayanheidari01@gmail.com>
2025-09-21 15:50:24 +0330

    Fixing the issue of VPN Mode not working in the latest version of Flutter

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -19,6 +19,12 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    packagingOptions {
+        jniLibs {
+            useLegacyPackaging = true
+            }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.example"
